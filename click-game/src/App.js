ce0d7ff024a7e2d8e7b1c57ec144b734dc849189
Michael Garland <michaelcgarland@gmail.com>
2018-06-23 10:04:06 -0400

    game operates

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -10,47 +10,60 @@ import './App.css';
 class App extends Component {
 
   state = {
-    playingCards
+    message: "Click a wrestler to begin.",
+    playingCards,
+    curScore: 0,
+    topScore: 0
   }
 
-  
+  shufflePictures = (picState) => {
+
+    picState.sort(() => .5 - Math.random());
+
+  }
+
   handleClick = (clicked, id) => {
     if (clicked) {
       console.log("you lose broooosis");
       const clicks = this.state.playingCards;
       clicks.map(card => card.clicked = false)
       this.setState({
+        message: "You guessed wrong!",
+        topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+        curScore: 0,
         clicks,
       })
       console.log(this.state);
-      
-      
-      
-
     } else {
       console.log('click again');
       const picture = this.state.playingCards;
       console.log(`this is what was clicked: ${JSON.stringify(picture[id - 1])}`);
-      picture[id - 1].clicked = true;
-      // picture.sort((a,b) => .5 - Math.random());
+      this.state.playingCards.forEach(item => {
+        if (item.id === id) {
+          item.clicked = true
+        }
+      });
+      this.shufflePictures(picture);
       this.setState({
+        message: "Keep guessing!",
+        curScore: this.state.curScore + 1,
         picture,
       })
       console.log(this.state);
-      
     }
-
-
-
   }
 
   render() {
     return (
       <div>
-        <NavBar />
-        <Title />
+        <NavBar
+          score={this.state.curScore}
+          highScore={this.state.topScore}
+        />
+        <Title
+          message={this.state.message}
+        />
         <Wrapper>
-
           {this.state.playingCards.map(cards => (
             <PlayingCard
               image={cards.image}
@@ -60,7 +73,6 @@ class App extends Component {
               handleClick={this.handleClick}
             />
           ))}
-
         </Wrapper>
         <Footer />
       </div>
--- click-game/src/componenets/Title/Title.js
+++ click-game/src/componenets/Title/Title.js
@@ -6,6 +6,7 @@ const Title = props => (
             <div className="directionContent">
                 <h1>Click a Picture to Play the game.</h1>
                 <h3>You lose if you click the same image twice!</h3>
+                <h3>{props.message}</h3>
             </div>
         </div>
 );
